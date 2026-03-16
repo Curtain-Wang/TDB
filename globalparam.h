df760864bdd6a67c06916607e04b2682180b178b
Curtain-Wang <wangqt1997@qq.com>
2026-03-16 13:05:25 +0800

    电流KB可设置

--- globalparam.h
+++ globalparam.h
@@ -2,7 +2,7 @@
 #define GLOBALPARAM_H
 #include <QHash>
 #define NO_WARN_PROT_STR    "无告警、保护"
-#define TITLE "DC100DC500P10K-V1.0.0"
+#define TITLE "DC100DC500P10K-V1.0.1"
 class QByteArray;
 class QString;
 class MainWindow;
