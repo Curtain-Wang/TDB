c95021d7fdfd925e8b06dc482469728671459129
Curtain-Wang <wangqt1997@qq.com>
2026-03-16 11:57:16 +0800

    电流K、B可设置

--- tformcali.cpp
+++ tformcali.cpp
@@ -183,3 +183,79 @@ void TFormCali::on_pushButton_9_clicked()
 }
 
 
+
+void TFormCali::on_pushButton_10_clicked()
+{
+    if(connFlag == 0)
+    {
+        QMessageBox::information(this, tr("提示"), tr("请先建立连接!"));
+        return;
+    }
+
+    float fValue = ui->l22->text().toFloat();
+    quint16 iValue= (quint16)(fValue * 1000 + 0.5);
+    quint16 addr = 22 + 1024;
+    quint8 startHigh = ((addr >> 8) & 0xFF);
+    quint8 startLow = (addr & 0xFF);
+    quint8 valueHigh = ((iValue >> 8) & 0xFF);
+    quint8 valueLow = (iValue & 0xFF);
+    mainwindow->manualWriteOneCMDBuild(startHigh, startLow, valueHigh, valueLow);
+}
+
+
+void TFormCali::on_pushButton_12_clicked()
+{
+    if(connFlag == 0)
+    {
+        QMessageBox::information(this, tr("提示"), tr("请先建立连接!"));
+        return;
+    }
+
+    float fValue = ui->l20->text().toFloat();
+    quint16 iValue= (quint16)(fValue * 1000 + 0.5);
+    quint16 addr = 20 + 1024;
+    quint8 startHigh = ((addr >> 8) & 0xFF);
+    quint8 startLow = (addr & 0xFF);
+    quint8 valueHigh = ((iValue >> 8) & 0xFF);
+    quint8 valueLow = (iValue & 0xFF);
+    mainwindow->manualWriteOneCMDBuild(startHigh, startLow, valueHigh, valueLow);
+}
+
+
+void TFormCali::on_pushButton_11_clicked()
+{
+    if(connFlag == 0)
+    {
+        QMessageBox::information(this, tr("提示"), tr("请先建立连接!"));
+        return;
+    }
+
+    float fValue = ui->l21->text().toFloat();
+    quint16 iValue= (quint16)(fValue * 1000 + 0.5);
+    quint16 addr = 21 + 1024;
+    quint8 startHigh = ((addr >> 8) & 0xFF);
+    quint8 startLow = (addr & 0xFF);
+    quint8 valueHigh = ((iValue >> 8) & 0xFF);
+    quint8 valueLow = (iValue & 0xFF);
+    mainwindow->manualWriteOneCMDBuild(startHigh, startLow, valueHigh, valueLow);
+}
+
+
+void TFormCali::on_pushButton_13_clicked()
+{
+    if(connFlag == 0)
+    {
+        QMessageBox::information(this, tr("提示"), tr("请先建立连接!"));
+        return;
+    }
+
+    float fValue = ui->l19->text().toFloat();
+    quint16 iValue= (quint16)(fValue * 1000 + 0.5);
+    quint16 addr = 19 + 1024;
+    quint8 startHigh = ((addr >> 8) & 0xFF);
+    quint8 startLow = (addr & 0xFF);
+    quint8 valueHigh = ((iValue >> 8) & 0xFF);
+    quint8 valueLow = (iValue & 0xFF);
+    mainwindow->manualWriteOneCMDBuild(startHigh, startLow, valueHigh, valueLow);
+}
+
